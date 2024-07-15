6345fd316e8da9b3ee3741084047c0de0daa928b
Jahnvi Aghera <jahnvi.aghera111599@marwadiuniversity.ac.in>
2024-07-15 16:19:17 +0530

    chore: Update C/C++ build tasks and settings

--- 0715/ex_01.cpp
+++ 0715/ex_01.cpp
@@ -1,3 +1,66 @@
 //EXPERIMENT NO.:
 //AIM:
 #include <iostream>
+using namespace std;
+int main()
+{
+    // string name,surname;
+    // char intial;
+    // cout<<"Enter your name: ";
+    // cin>>name;
+    // cout<<"Enter your intial: ";
+    // cin>>intial;
+    // cout<<"Enter your surname: ";
+    // cin>>surname;
+    // cout<<"Your name is: "<<name<<" "<<intial<<" "<<surname;
+
+
+    //----------------------------------------------
+
+    // int a;
+    // char c = 'C';
+    // // cout<<"Enter a character: ";
+    // // cin>>c;
+    // cout<<"Typecasted output is : "<<(int)c;
+    // return 0;
+
+    //----------------------------------------------
+
+    // int a;
+    // float f;
+    // cout<<"Enter a integer: ";
+    // cin>>a;
+    // cout<<"Typecasted output(int to float) is : "<<(float)a<<endl;
+    // cout<<"Typecasted output(float to int) is : "<<(int)(float)a;
+
+    //----------------------------------------------
+
+    // int a = 10;
+    // if (a%2==0)
+    // {
+    //     cout<<"Even";
+    // }else if (a==1)
+    // {
+    //     cout<<"the number is 1";
+    // }
+    // else
+    // {
+    //     cout<<"Odd";
+    // }
+
+    //----------------------------------------------
+
+    // int a;
+    // cout<<"Enter a number: ";
+    // cin>>a;
+    // if(a>0){
+    //     cout<<"the number is positive";
+    // }else if (a==0)
+    // {
+    //     cout<<"the number is zero";
+    // }else{
+    //     cout<<"the number is negative";
+    // }
+    
+    return 0;
+}
