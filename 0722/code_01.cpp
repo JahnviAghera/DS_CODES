f480e500b804616b909ab0cb3008aa39fe481859
Jahnvi Aghera <jahnvi.aghera111599@marwadiuniversity.ac.in>
2024-07-22 16:07:00 +0530

    Refactor employee class to encapsulate salary and add getter and setter methods

--- 0722/code_01.cpp
+++ 0722/code_01.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
 using namespace std;
 class employee{
+    double salary;
 public:
     string name;
     char gender;
-    double salary;
     int empID;
+    
+    void setSalary(double s){
+        salary = s;
+    }
+    double getSalary(){
+        return salary;
+    }
 };    
 int main() {
     employee emp1;
     emp1.name = "Jahnvi";
     emp1.gender = 'F';
-    emp1.salary = 50000;
+    emp1.setSalary(50000);
     emp1.empID = 101;
 
     cout << "Name: " << emp1.name << endl;
     cout << "Gender: " << emp1.gender << endl;
-    cout << "Salary: " << emp1.salary << endl;
+    cout << "Salary: " << emp1.getSalary() << endl;
     cout << "Employee ID: " << emp1.empID << endl;
     return 0;
 }
